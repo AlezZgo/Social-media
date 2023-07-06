339c2138c57eeba3c6ac8cbd9a5dc3804c5da8c7
Александр <alez.grante@gmail.com>
2023-07-06 16:31:17 +0300

    edge to edge feature

--- app/src/main/java/com/alezzgo/socialmedia/MainActivity.kt
+++ app/src/main/java/com/alezzgo/socialmedia/MainActivity.kt
@@ -4,16 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.FavoriteBorder
-import androidx.compose.material3.AssistChip
-import androidx.compose.material3.AssistChipDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -24,47 +14,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 import com.alezzgo.socialmedia.ui.theme.SocialMediaTheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
-import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
             SocialMediaTheme {
-                TransparentSystemBars()
-                Box(Modifier.windowInsetsPadding(WindowInsets.statusBars)) {
-
-                }
-
 
             }
-
-
         }
-    }
-}
-
-@Composable
-fun TransparentSystemBars() {
-    val systemUiController = rememberSystemUiController()
-    val useDarkIcons = !isSystemInDarkTheme()
-    val BlackScrim = Color(0f, 0f, 0f, 0.3f) // 30% opaque black
-
-    DisposableEffect(systemUiController, useDarkIcons) {
-        systemUiController.setSystemBarsColor(
-            color = Color.Transparent,
-            darkIcons = useDarkIcons,
-            isNavigationBarContrastEnforced = false,
-            transformColorForLightContent = { original ->
-                BlackScrim.compositeOver(original)
-            }
-        )
-
-        onDispose {}
     }
 }
 
