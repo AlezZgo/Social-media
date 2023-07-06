efc1a01eb350a5e9a303cc08863ee2beb2f0ea65
Александр <alez.grante@gmail.com>
2023-07-06 12:17:27 +0300

    Initial commit 2

--- app/src/main/java/com/alezzgo/socialmedia/MainActivity.kt
+++ app/src/main/java/com/alezzgo/socialmedia/MainActivity.kt
@@ -3,29 +3,68 @@ package com.alezzgo.socialmedia
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.compositeOver
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.alezzgo.socialmedia.ui.theme.SocialMediaTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             SocialMediaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                TransparentSystemBars()
+                Box(Modifier.windowInsetsPadding(WindowInsets.statusBars)) {
+
                 }
+
+
             }
+
+
         }
+    }
+}
+
+@Composable
+fun TransparentSystemBars() {
+    val systemUiController = rememberSystemUiController()
+    val useDarkIcons = !isSystemInDarkTheme()
+    val BlackScrim = Color(0f, 0f, 0f, 0.3f) // 30% opaque black
+
+    DisposableEffect(systemUiController, useDarkIcons) {
+        systemUiController.setSystemBarsColor(
+            color = Color.Transparent,
+            darkIcons = useDarkIcons,
+            isNavigationBarContrastEnforced = false,
+            transformColorForLightContent = { original ->
+                BlackScrim.compositeOver(original)
+            }
+        )
+
+        onDispose {}
     }
 }
 
--- app/src/main/java/com/alezzgo/socialmedia/ui/theme/Theme.kt
+++ app/src/main/java/com/alezzgo/socialmedia/ui/theme/Theme.kt
@@ -9,38 +9,82 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val LightColors = lightColorScheme(
+    primary = Light_primary,
+    onPrimary = Light_onPrimary,
+    primaryContainer = Light_primaryContainer,
+    onPrimaryContainer = Light_onPrimaryContainer,
+    secondary = Light_secondary,
+    onSecondary = Light_onSecondary,
+    secondaryContainer = Light_secondaryContainer,
+    onSecondaryContainer = Light_onSecondaryContainer,
+    tertiary = Light_tertiary,
+    onTertiary = Light_onTertiary,
+    tertiaryContainer = Light_tertiaryContainer,
+    onTertiaryContainer = Light_onTertiaryContainer,
+    error = Light_error,
+    errorContainer = Light_errorContainer,
+    onError = Light_onError,
+    onErrorContainer = Light_onErrorContainer,
+    background = Light_background,
+    onBackground = Light_onBackground,
+    surface = Light_surface,
+    onSurface = Light_onSurface,
+    surfaceVariant = Light_surfaceVariant,
+    onSurfaceVariant = Light_onSurfaceVariant,
+    outline = Light_outline,
+    inverseOnSurface = Light_inverseOnSurface,
+    inverseSurface = Light_inverseSurface,
+    inversePrimary = Light_inversePrimary,
+    surfaceTint = Light_surfaceTint,
+    outlineVariant = Light_outlineVariant,
+    scrim = Light_scrim,
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val DarkColors = darkColorScheme(
+    primary = Dark_primary,
+    onPrimary = Dark_onPrimary,
+    primaryContainer = Dark_primaryContainer,
+    onPrimaryContainer = Dark_onPrimaryContainer,
+    secondary = Dark_secondary,
+    onSecondary = Dark_onSecondary,
+    secondaryContainer = Dark_secondaryContainer,
+    onSecondaryContainer = Dark_onSecondaryContainer,
+    tertiary = Dark_tertiary,
+    onTertiary = Dark_onTertiary,
+    tertiaryContainer = Dark_tertiaryContainer,
+    onTertiaryContainer = Dark_onTertiaryContainer,
+    error = Dark_error,
+    errorContainer = Dark_errorContainer,
+    onError = Dark_onError,
+    onErrorContainer = Dark_onErrorContainer,
+    background = Dark_background,
+    onBackground = Dark_onBackground,
+    surface = Dark_surface,
+    onSurface = Dark_onSurface,
+    surfaceVariant = Dark_surfaceVariant,
+    onSurfaceVariant = Dark_onSurfaceVariant,
+    outline = Dark_outline,
+    inverseOnSurface = Dark_inverseOnSurface,
+    inverseSurface = Dark_inverseSurface,
+    inversePrimary = Dark_inversePrimary,
+    surfaceTint = Dark_surfaceTint,
+    outlineVariant = Dark_outlineVariant,
+    scrim = Dark_scrim,
 )
 
 @Composable
 fun SocialMediaTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -49,11 +93,11 @@ fun SocialMediaTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        darkTheme -> DarkColors
+        else -> LightColors
     }
     val view = LocalView.current
+
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
