77f323283f49cd60d761cb8a99fce22a9d4a6c2c
Александр <alez.grante@gmail.com>
2023-07-24 19:49:52 +0300

    unknown fix

--- app/src/main/java/com/alezzgo/socialmedia/ui/screens/WorkshopScreen.kt
+++ app/src/main/java/com/alezzgo/socialmedia/ui/screens/WorkshopScreen.kt
@@ -1,9 +1,12 @@
 package com.alezzgo.socialmedia.ui.screens
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -14,8 +17,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.NonRestartableComposable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.alezzgo.socialmedia.ui.ext.horizontalCutoutPadding
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.tynkovski.privateMessenger.utils.ext.plus
 
 @OptIn(ExperimentalMaterial3Api::class)
 @NonRestartableComposable
@@ -26,8 +31,14 @@ fun WorkshopScreen(
 ) = Scaffold(
     topBar = { TopAppBar(title = { Text("Workshop") }) }
 ) {
-    LazyColumn(modifier = Modifier.fillMaxSize(), contentPadding = it) {
-
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .navigationBarsPadding()
+            .horizontalCutoutPadding(),
+        contentPadding = it + PaddingValues(8.dp)
+    ) {
         item {
             repeat(100) {
                 Button(
